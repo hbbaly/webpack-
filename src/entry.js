cfe13533ea52193b8fe358faef0e6197174a163c
hbbaly <1422699902@qq.com>
2018-08-09 23:30:38 +0800

    webpack模板

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+ require("./css/main.css")
+//  require('./css/main.scss')
+ require("./css/main.less")
+ let [a,b] = [1,2]
+document.write("<h1>hello webpack-dev-server</h1>")
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require("path")
+const glob = require("glob")
+const purifyCSS = require("purifycss-webpack")
+const webpack = require("webpack")
+const uglify = require('uglifyjs-webpack-plugin')
+const htmlPlugin = require('html-webpack-plugin')
+const extractText = require('extract-text-webpack-plugin')
+const website = {
+    publicPath:'http://localhost:3000/'
+}
+module.exports = {
+    entry:"./src/entry.js",
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js',
+        publicPath:website.publicPath
+    },
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'localhost',
+        compress:true,
+        port:3000
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:extractText.extract({
+                    fallback:'style-loader',
+                    use:[
+                        {
+                            loader:'css-loader',
+                            options:{
+                                importLoader:1
+                            }
+                        },
+                       {loader:"postcss-loader"}
+                    ],
+                    
+                   
+                })
+            },{
+                test:/\.(png|jpg|gif)$/,
+                use:[
+                    {
+                        loader:'url-loader',    
+                        options:{
+                            limit:50000,
+                            outputPath:'img/'
+                        }
+                    }
+                ]
+            },{
+                test:/\.(htm|html)$/i,
+                use:['html-withimg-loader']
+            },{
+                test:/\.less$/,
+                use:extractText.extract({
+                    use:[
+                        {loader:'css-loader'},
+                        {loader:'less-loader'}
+                    ],
+                    fallback:'style-loader'
+                })
+            },{
+                test:/\.sass$/,
+                use:extractText.extract({
+                    use:[
+                        {loader:'css-loader'},
+                        {loader:'sass-loader'}
+                    ],
+                    fallback:'style-loader'
+                })
+                
+            },{
+                test:/\.js$/,
+                use:{loader:'babel-loader'},
+                exclude:/node_modules/
+            }
+            
+        ]
+    },
+    plugins:[
+        new uglify(),
+        new htmlPlugin({
+            minify:{
+                removeAttributeQuotes:true
+            },
+            hash:true,
+            template:'./src/index.html'
+        }),
+        new extractText("css/main.css"),
+        new purifyCSS({
+            paths:glob.sync(path.join(__dirname,'src/*.html'))
+        })
+    ]
+       
+    
+}

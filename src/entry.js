9c364373c83e0bc3e381dd05b6dffbc010c11fd4
hbbaly <1422699902@qq.com>
2018-08-10 18:17:58 +0800

    pro

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,6 @@
  require("./css/main.css")
 //  require('./css/main.scss')
- require("./css/main.less")
+//  require("./css/main.less")
  let [a,b] = [1,2]
+ console.log(a,b)
 document.write("<h1>hello webpack-dev-server</h1>")
